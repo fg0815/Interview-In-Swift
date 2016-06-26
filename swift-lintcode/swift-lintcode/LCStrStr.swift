f166518b75b55e5fd4e696706fbfbdbdfb3319e9
Jingwei Huang <Jingwei.Huang@auspost.com.au>
2016-06-26 19:04:51 +1000

    Added ststr

---
+++ swift-lintcode/swift-lintcode/LCStrStr.swift
@@ -0,0 +1,38 @@
+//
+//  LCStrStr.swift
+//  swift-lintcode
+//
+//  Created by Jingwei Huang on 26/06/2016.
+//  Copyright © 2016 Core Apps Pty Ltd. All rights reserved.
+//
+
+import Foundation
+
+// http://www.lintcode.com/en/problem/strstr/
+class LCStrStr: NSObject {
+    /**
+     * Returns a index to the first occurrence of target in source,
+     * or -1  if target is not part of source.
+     * @param source string to be scanned.
+     * @param target string containing the sequence of characters to match.
+     */
+    class func strStr(source: String, target: String) -> Int {
+        for i in 0..<source.characters.count - target.characters.count + 1 {
+            var count = 0
+            for j in 0..<target.characters.count {
+                count = j
+                if source[source.startIndex.advancedBy(i+j)] != target[target.startIndex.advancedBy(j)] {
+                    break
+                }
+            }
+            
+            if count + 1 == target.characters.count {
+                return i
+            }
+            
+        }
+        
+        return -1
+        
+    }
+}
---
+++ swift-lintcode/swift-lintcodeTests/LCStrStrTest.swift
@@ -0,0 +1,38 @@
+//
+//  LCStrStrTest.swift
+//  swift-lintcode
+//
+//  Created by Jingwei Huang on 26/06/2016.
+//  Copyright © 2016 Core Apps Pty Ltd. All rights reserved.
+//
+
+import XCTest
+
+// If source = "source" and target = "target", return -1.
+// If source = "abcdabcdefg" and target = "bcd", return 1.
+
+class LCStrStrTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testStrStr() {
+        let source1 = "source"
+        let target1 = "target"
+        XCTAssertTrue(LCStrStr.strStr(source1, target: target1) == -1)
+        
+        let source2 = "abcdabcdefg"
+        let target2 = "bcd"
+        XCTAssertTrue(LCStrStr.strStr(source2, target: target2) == 1)
+    }
+
+    
+
+}
