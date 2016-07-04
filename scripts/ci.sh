9e2bef8c23cb6cb4560aa7e0b65b0a6be7de325c
Jingwei Huang <Jingwei.Huang@auspost.com.au>
2016-07-04 14:15:44 +1000

    Updated build scripts

--- scripts/ci.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-xcodebuild -project swift-lintcode.xcodeproj -scheme "swift-lintcode" -destination "platform=iOS Simulator,name=iPhone 6" test
---
+++ swift-lintcodeTests/LCTreeNodeHelperTest.swift
@@ -0,0 +1,13 @@
+//
+//  LCTreeNodeHelperTest.swift
+//  swift-lintcode
+//
+//  Created by Jingwei Huang on 3/07/2016.
+//  Copyright © 2016 Core Apps Pty Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class LCTreeNodeHelperTest: NSObject {
+
+}
