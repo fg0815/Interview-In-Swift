56f7c56618f3d138b48b72b485d1da7830869275
Jingwei Huang <Jingwei.Huang@auspost.com.au>
2016-07-12 23:25:43 +1000

    Added the same tree for linkedin

--- InterviewInSwift/Linkedin/LKISameTree.swift
+++ InterviewInSwift/Linkedin/LKISameTree.swift
@@ -6,8 +6,12 @@
 //  Copyright © 2016 Core Apps Pty Ltd. All rights reserved.
 //
 
-import Cocoa
+import Foundation
 
+// Refer to `LC100SameTree`
+// Same tree, mirror tree
 class LKISameTree: NSObject {
-
+    class func isSameTree(p: LCTreeNode?, q: LCTreeNode?) -> Bool {
+        return LC100SameTree.isSameTree(p, q: q)
+    }
 }
