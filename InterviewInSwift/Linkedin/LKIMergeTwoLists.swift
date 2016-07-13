7730418ed69434839d50fb2e46ddc8595efc07d8
Jingwei Huang <Jingwei.Huang@auspost.com.au>
2016-07-13 19:33:22 +1000

    Adde merge two lists for linkedin

---
+++ InterviewInSwift/Linkedin/LKIMergeTwoLists.swift
@@ -0,0 +1,15 @@
+//
+//  LKIMergeTwoLists.swift
+//  InterviewInSwift
+//
+//  Created by Jingwei Huang on 13/07/2016.
+//  Copyright © 2016 Core Apps Pty Ltd. All rights reserved.
+//
+
+import Foundation
+
+class LKIMergeTwoLists: NSObject {
+    class func mergeTwoLists(l1: LCLinkedListNode, l2: LCLinkedListNode) -> LCLinkedListNode {
+        return LCMergeTwoLists.mergeTwoLists(l1, l2: l2)
+    }
+}
